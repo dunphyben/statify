51cc6bf966e53267c6fd4a2eacbd28211b869382
Josh Fowler & Benjamin Paul Dunphy <student@epicodus.com>
2014-04-30 10:08:02 -0700

    Add newTeam form and nest routes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
-App = Ember.Application.create();
+App = Ember.Application.create({
+  LOG_TRANSITIONS: true
+});
 
 App.Router.map(function() {
-  this.resource('index', { path: '/' });
+  this.resource('index', { path: '/' }, function() {
+    this.resource('newTeam', { path: '/teams/new' });
+  });
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return ['Bulls', 'Blazers'];
   }
 });
+
+
+App.NewTeamController = Ember.ObjectController.extend({
+  actions:{
+    createTeam: function() {
+      var model = this.get('model');
+      model.save();
+    }
+  }
+})
